838ae354af51aede72c6be622b8fbd4cd7710d86
lallnoqfan <lallnoqfan@gmail.com>
2023-09-22 20:38:27 +0300

    separating data models to db module

--- models.py
+++ models.py
@@ -1,34 +1,8 @@
 from os import getenv
-from typing import List, Tuple, Any, Dict
+from typing import Any
 
 from dotenv import find_dotenv, load_dotenv
 from pydantic import BaseModel
-
-# todo: rework as pandas models one day
-
-
-class Player(BaseModel):
-    name:      str
-    color_hex: str
-    color_rgb: Tuple[int, int, int]
-    tiles:     List[str] = []
-
-
-class RollBase(BaseModel):
-    player:   Player
-    post_num: int
-
-
-class GameData(BaseModel):
-    board:       str = 'b'
-    thread:      str = ''
-    last_number: int = 1
-    paste:       str = ''
-
-    players:     List[Player] = []
-    roll_bases:  List[RollBase] = []
-
-    cookies: Dict[str, str] = dict()
 
 
 class Usercode(BaseModel):
---
+++ modules/db/__init__.py
@@ -0,0 +1,1 @@
+from .models import GameData, Player, RollBase
