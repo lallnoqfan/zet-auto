92e24a1b6e0e41ae62e7abb6c31ecc0d9b4abb03
lallnoqfan <lallnoqfan@gmail.com>
2023-09-23 19:12:57 +0300

    launch setup

--- __main__.py
+++ __main__.py
@@ -1,8 +1,14 @@
-from app import ArgsParser as App
+from sys import argv
 
 
 if __name__ == '__main__':
+
+    if len(argv) > 1:
+        from app import ArgsParser as App
+    else:
+        from app import InquirerApp as App
+
     try:
-        App()
+        App.run()
     except KeyboardInterrupt:
         print("\nStopped by keyboard")
