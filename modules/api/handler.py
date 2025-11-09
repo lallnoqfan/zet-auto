efde86e5bf146b8b1639203920403f5b8b70af73
lallnoqfan <lallnoqfan@gmail.com>
2025-11-09 15:27:39 +0300

    minor: setting headers/proxies for api requests & printing responses

--- modules/api/handler.py
+++ modules/api/handler.py
@@ -11,11 +11,16 @@ class DvachAPIHandler:
 
     def __init__(self, usercode: str, usercode_auth: str, passcode_auth: str,
                  use_proxy: bool = False, proxy: str = None):
-
         self.usercode = usercode
         self.cookies = {
             'usercode_auth': usercode_auth,
             'passcode_auth': passcode_auth,
+        }
+
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:144.0) Gecko/20100101 Firefox/144.0',
+            "X-Requested-With": "XMLHttpRequest",
+            "Accept": "application/json, text/javascript, */*; q=0.01",
         }
 
         self.use_proxy = use_proxy
@@ -28,7 +33,6 @@ class DvachAPIHandler:
         self.cookies.update(cookies)
 
     def get_thread(self, board: str, thread_num: str | int) -> DvachThread | None:
-
         r = self.get_thread_raw(board, thread_num)
 
         if r.status_code != 200:
@@ -50,21 +54,23 @@ class DvachAPIHandler:
         url = f'{DvachConfig.BASE_URL}/{board}/res/{thread_num}.json'
 
         if not self.use_proxy:
-            r = get(url)
+            r = get(url, cookies=self.cookies, headers=self.headers)
         else:
-            r = get(url, proxies=self.proxies)
+            r = get(url, cookies=self.cookies, headers=self.headers, proxies=self.proxies)
+
+        print(f"GET - {url} - {r}")
 
         return r
 
-    def post_posting(self, schema: DvachPostingSchemaIn,
-                     headers: Dict = None, cookies: Dict = None) -> Response:
-        url = f'{DvachConfig.BASE_URL}/user/posting'
+    def post_posting(self, schema: DvachPostingSchemaIn, headers: Dict = None, cookies: Dict = None) -> Response:
+        url = f'{DvachConfig.BASE_URL}/user/posting?nc=1'
 
         schema.usercode = self.usercode
         data = schema.to_data()
 
         if not headers:
-            headers = {}
+            headers = dict()
+        headers.update(self.headers)
         headers.update({'Content-Type': data.content_type})
 
         if not cookies:
@@ -75,5 +81,11 @@ class DvachAPIHandler:
             r = post(url, data=data, headers=headers, cookies=cookies)
         else:
             r = post(url, data=data, headers=headers, cookies=cookies, proxies=self.proxies)
+
+        print(f"POST - {url} - {r}")
+        print(data)
+        print(r.request.headers)
+        print(r)
+        print()
 
         return r
